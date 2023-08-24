480ff9189de3998f2e3503a142b91df0318e7353
Renato Batista <zenatuz@gmail.com>
2023-08-24 15:26:46 +0100

    load brew in osx

--- .install.sh
+++ .install.sh
@@ -59,8 +59,10 @@ echo ""
 echo "Loading Brew"
 echo 
 #############################################
+
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -d /opt/homebrew/ && eval "$(/opt/homebrew/bin/brew shellenv)"
 brew -v
 
 echo ""
